9263052c1954d80a7bc519fe5edb261cbfee8af2
themaster-template <same7163@gmail.com>
2026-06-02 21:15:20 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'my-blog-v1';
+const ASSETS = [
+  '/'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
